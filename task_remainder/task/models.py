2329ef67c8bb4fc102ae475b77662996d2273177
Rithesh Rohan <ritheshrohan7@gmail.com>
2017-06-16 14:14:44 +0530

    Revert "removing stuff"
    This reverts commit b0aa74bc11a685f4f49b8406362dea57af109723.

---
+++ task_remainder/task/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Person(models.Model):
+    user    = models.ForeignKey(User)
+    
+    def __unicode__(self):
+    	return self.user.username
+
+    class Meta:
+    	app_label="task"
+    		
+class Task(models.Model):
+    title           = models.CharField(max_length=50, default="")
+    # reminder_time   = models.TimeField(blank=True,null=True,help_text="Time format is :HH:MM:SS")
+    person          = models.ForeignKey(Person,related_name="task_user")
+
+    def __unicode__(self):
+        return "Task is {}".format(self.title)
+
+    class Meta:
+        app_label="task"
+        db_table="task" 
+    
