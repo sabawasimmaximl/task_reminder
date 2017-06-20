da55dafd779a7f6332686124a233b78448682d67
Rithesh Rohan <ritheshrohan7@gmail.com>
2017-06-20 18:55:40 +0530

    adding auth token to header and token exists conditions

--- Client/src/app/Services/SyncService/sync-service.service.js
+++ Client/src/app/Services/SyncService/sync-service.service.js
@@ -8,13 +8,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var router_1 = require("@angular/router");
 var http_1 = require("@angular/http");
 var core_1 = require("@angular/core");
 require("rxjs/add/operator/toPromise");
 var SyncService = (function () {
-    function SyncService(http) {
+    function SyncService(http, router) {
         this.http = http;
+        this.router = router;
         this.headers = new http_1.Headers({ 'Content-Type': 'application/json' });
+        this.token = localStorage.getItem('auth_token');
+        if (this.token) {
+            this.headers.append('Authorization', 'Bearer ' + this.token);
+            console.log("TOKEN EXISTS----------");
+            console.log(this.headers);
+            console.log("Navigating to users----------");
+            this.router.navigate(['dashboard']);
+        }
+        else {
+            console.log("TOKEN DOES NOT EXIST");
+            console.log("Navigating to users----------");
+            this.router.navigate(['login']);
+        }
     }
     SyncService.prototype.handleError = function (error) {
         console.error('An error has occurred', error); // for demo purposes only
@@ -22,25 +37,37 @@ var SyncService = (function () {
     };
     SyncService.prototype.retrieve = function (url, operation) {
         console.log("Operation = ", operation);
-        return this.http.get(url).toPromise();
+        if (this.token)
+            return this.http.get(url, this.token).toPromise();
+        else {
+            console.log("TOKEN DOES NOT EXIST");
+        }
     };
     SyncService.prototype.assigntask = function (createTaskUrl, title, person, operation) {
         console.log("Operation = ", operation);
-        return this.http.post(createTaskUrl, JSON.stringify({ title: title, person: person }), { headers: this.headers })
-            .toPromise();
+        if (this.token)
+            return this.http.post(createTaskUrl, JSON.stringify({ title: title, person: person }), { headers: this.headers })
+                .toPromise();
+        else {
+            console.log("TOKEN DOES NOT EXIST");
+        }
     };
     SyncService.prototype.getSingleUser = function (url, operation) {
         console.log("Operation = ", operation);
-        return this.http.get(url)
-            .toPromise()
-            .then(function (response) { return response.json(); })
-            .catch(this.handleError);
+        if (this.token)
+            return this.http.get(url, this.token)
+                .toPromise()
+                .then(function (response) { return response.json(); })
+                .catch(this.handleError);
+        else {
+            console.log("TOKEN DOES NOT EXIST");
+        }
     };
     return SyncService;
 }());
 SyncService = __decorate([
     core_1.Injectable(),
-    __metadata("design:paramtypes", [http_1.Http])
+    __metadata("design:paramtypes", [http_1.Http, router_1.Router])
 ], SyncService);
 exports.SyncService = SyncService;
 //# sourceMappingURL=sync-service.service.js.map
--- Client/src/app/Services/SyncService/sync-service.service.ts
+++ Client/src/app/Services/SyncService/sync-service.service.ts
@@ -1,4 +1,4 @@
-
+import {Router} from '@angular/router';
 import { Headers, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
@@ -11,7 +11,6 @@ import { LoginComponent }   from '../../Components/LoginComponent/login.componen
 //Services
 
 import { UserService }          from '../../Services/UserService/user.service';
-import { UserSearchService }    from '../../Services/UserSearchService/user-search.service';
 import { TaskService }          from '../../Services/TaskService/task.service';
 // import { AuthService }          from '../../Services/AuthService/auth-service.service';
 
@@ -24,11 +23,27 @@ import { User } from '../../Class/user';
 @Injectable()
 export class SyncService {
 
-    
-  private headers = new Headers({'Content-Type': 'application/json'});
+    private token:string;
+  private headers = new Headers(
+    {'Content-Type': 'application/json'});
 
   
-  constructor(private http:Http){
+  constructor(private http:Http,private router:Router){
+      
+      this.token=localStorage.getItem('auth_token');
+        
+      if(this.token) {
+          this.headers.append('Authorization', 'Bearer ' + this.token);
+          console.log("TOKEN EXISTS----------");
+          console.log(this.headers);
+          console.log("Navigating to users----------");
+          this.router.navigate(['dashboard'])
+      }
+        else{
+          console.log("TOKEN DOES NOT EXIST");
+           console.log("Navigating to users----------");
+          this.router.navigate(['login'])
+        }
   }
 
   public handleError(error: any): Promise<any> {
@@ -42,26 +57,39 @@ export class SyncService {
   retrieve(url:string,operation:string){
 
     console.log("Operation = ", operation);
-    return this.http.get(url).toPromise();
+    if(this.token)
+    return this.http.get(url,this.token).toPromise();
+    else{
+          console.log("TOKEN DOES NOT EXIST");
+          
+        }
   
 }
 
   assigntask(createTaskUrl:string,title:string,person:number,operation:string){
     
     console.log("Operation = ", operation);
-    
+    if(this.token)
     return this.http.post(createTaskUrl, JSON.stringify({title,person}), {headers: this.headers})
                     .toPromise();
+    else{
+          console.log("TOKEN DOES NOT EXIST");
+          
+        }                
 
   }
 
   getSingleUser(url:string,operation:string){
     console.log("Operation = ", operation);
-
-  return this.http.get(url)
+    if(this.token)
+    return this.http.get(url,this.token)
     .toPromise()
     .then(response => response.json())
     .catch(this.handleError);
+    else{
+          console.log("TOKEN DOES NOT EXIST");  
+        }
+
   }
 
 }
