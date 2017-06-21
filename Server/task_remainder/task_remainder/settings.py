6b2a2d13a4ed1682715bf0fc345248c0e6a08b6c
djangoconfire <djangopycon@gmail.com>
2017-06-21 13:27:53 +0530

    updated settings

--- Server/task_remainder/task_remainder/settings.py
+++ Server/task_remainder/task_remainder/settings.py
@@ -147,19 +147,3 @@ REST_FRAMEWORK={
 
 
 }
-
-
-# Authentication checking
-
-'''
-curl -X POST -d "username=mark&password=m123456789" http://localhost:8000/api/auth/token/
-
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hcmsiLCJ1c2VyX2lkIjo1LCJlbWFpbCI6IiIsImV4cCI6MTQ5Nzk1NjczM30.8IUTgVGgwvUjiGjEF3lDphrDy0hhwArbJjg9Zll80uY
-
-curl -H "Authorization: JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hcmsiLCJ1c2VyX2lkIjo1LCJlbWFpbCI6IiIsImV4cCI6MTQ5Nzk1NjczM30.8IUTgVGgwvUjiGjEF3lDphrDy0hhwArbJjg9Zll80uY
-" http://localhost:8000/api/task/list/
-
-
-curl http://localhost:8000/api/task/list/
-
-'''
